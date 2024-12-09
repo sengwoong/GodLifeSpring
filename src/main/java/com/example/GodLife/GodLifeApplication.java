3997a0c34f309df90ebd04d1e30fe15c844826d2
sengwoong <rej287573@gmail.com>
2024-12-09 21:56:56 +0900

    :sparkles: mysql redis 세팅 완료

---
+++ src/main/java/com/example/GodLife/GodLifeApplication.java
@@ -0,0 +1,13 @@
+package com.example.GodLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodLifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GodLife/GodLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GodLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
